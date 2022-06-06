db801f7ed4f792dbf9592f999afa2f096233df4a
Sam Smith <romanmissilin@mail.ru>
2022-06-06 20:33:07 +0300

    Начало

---
+++ src/main/java/com/example/homework2_6/HomeWork26Application.java
@@ -0,0 +1,13 @@
+package com.example.homework2_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork26Application.class, args);
+	}
+
+}
